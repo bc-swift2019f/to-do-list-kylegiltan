b98277c4c279a93a8107937f4858ebaba29f5e46
Kyle <32943027+kylegiltan@users.noreply.github.com>
2019-09-21 13:43:24 -0400

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by Kyle Gil Tan on 9/21/19.
+//  Copyright © 2019 Kyle Gil Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
